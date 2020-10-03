4c91a2770af25e6ff23c3d204abf8d7fd04bbc68
Russell Hampton <russellhampton@gmail.com>
2020-10-02 20:33:26 -0400

    Adds prelim script for post-processing rexpaint output to be compatible w/ current master branch of Cataclysm

---
+++ rexpaint2cdda.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for f in $(ls *-CDDA.txt)
+do
+	echo Processing ${f}
+	sed -i 's/_>/>/g' ${f}
+
+	# Replacements where CDDA doesn't have the color pair
+	sed -i 's/color_white_dark_gray/color_white/g' ${f}
+	sed -i 's/color_white_light_gray/color_white/g' ${f}
+
+	cat ${f} >> data/json/_append_ascii_arts.json
+
+	# Trim >1 trailing blank line
+	#sed -i 's///g' ${f}
+	#sed -i 's/(\w+"\w+\n",)+\(\w+"\w+"\)/\1/g' ${f}
+
+	code ${f}
+done
