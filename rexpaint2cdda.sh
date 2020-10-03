ca81f5c14b8eb228e75c469bd212255e312a8191
Russell Hampton <russellhampton@gmail.com>
2020-10-03 09:10:18 -0400

    Comments out aggregate append ascii arts json file

--- rexpaint2cdda.sh
+++ rexpaint2cdda.sh
@@ -8,8 +8,9 @@ do
 	# Replacements where CDDA doesn't have the color pair
 	sed -i 's/color_white_dark_gray/color_white/g' ${f}
 	sed -i 's/color_white_light_gray/color_white/g' ${f}
+	sed -i 's/color_dark_gray_light_gray/color_dark_gray/g' ${f}
 
-	cat ${f} >> data/json/_append_ascii_arts.json
+	#cat ${f} >> data/json/_append_ascii_arts.json
 
 	# Trim >1 trailing blank line
 	#sed -i 's///g' ${f}
