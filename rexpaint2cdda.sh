67919c7582d0e3056815438e47bf071e1ec4bddc
Russell Hampton <russellhampton@gmail.com>
2020-10-04 19:11:10 -0400

    Updates rexpaint2cdda.sh with more swaps, adds grep command for id for each file processed

--- rexpaint2cdda.sh
+++ rexpaint2cdda.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-for f in $(ls *-CDDA.txt)
+# jq .id <(cat screwdriver-TESTING.txt) or cat screwdriver-CDDA.txt | jq .id
+
+if [ ! -z "$1" ]; then
+	filemask=$1
+else
+	filemask="*-CDDA.txt"
+fi
+
+for f in $(ls $filemask)
 do
-	echo Processing ${f}
 	sed -i 's/_>/>/g' ${f}
 
 	# Replacements where CDDA doesn't have the color pair
 	sed -i 's/color_white_dark_gray/color_white/g' ${f}
 	sed -i 's/color_white_light_gray/color_white/g' ${f}
 	sed -i 's/color_dark_gray_light_gray/color_dark_gray/g' ${f}
+	sed -i 's/color_light_gray_dark_gray/color_light_gray/g' ${f}
+	sed -i 's/color_yellow_dark_gray/color_yellow/g' ${f}
 
-	#cat ${f} >> data/json/_append_ascii_arts.json
+	sed -i 's/  }/  },/' ${f}
 
-	# Trim >1 trailing blank line
-	#sed -i 's///g' ${f}
-	#sed -i 's/(\w+"\w+\n",)+\(\w+"\w+"\)/\1/g' ${f}
+    itemid=$(sed 's/-CDDA\.txt//' <(echo ${f}))
+	CMD="egrep -riIn 'id.+:.+${itemid}' data/json/items"
+	eval $CMD
 
 	code ${f}
 done
